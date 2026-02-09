f357c6a69c4a66ffb237a7c57ea3d4572ab64bbe
Fazliddin <dilshodovfazliddin@gmail.com>
2026-02-09 17:07:49 +0500

    for grammar

--- src/main/java/com/uzum/transactionprocessing/constant/enums/TransactionStatus.java
+++ src/main/java/com/uzum/transactionprocessing/constant/enums/TransactionStatus.java
@@ -11,7 +11,7 @@ public enum TransactionStatus {
     RECEIVER_INFO_VALIDATION_FAILED(true),
     AMOUNT_VALIDATED(false),
     AMOUNT_VALIDATION_FAILED(true),
-    CALCULATE_COMPLETED_FAILED(true),
+    CALCULATE_FAILED(true),
     PROCESSING_BY_LEDGER(true),
     COMPLETED(true),
     FAILED(true);
